f209b9865f3d996dbeb169d72e053582d70ae201
anunirmani <anuttaranirmani@gmail.com>
2025-12-30 10:44:54 +0530

    added slide bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,22 +15,21 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <div class="min-h-screen bg-gray-100 flex">
+
+        {{-- Sidebar --}}
+        @include('layouts.sidebar')
+
+        {{-- Main content --}}
+        <div class="flex-1 ml-64">
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
+            <main class="p-6">
                 {{ $slot }}
             </main>
         </div>
-    </body>
+
+    </div>
+</body>
+
 </html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,41 @@
+
+<div class="w-64 h-screen bg-gray-900 text-white fixed left-0 top-0 flex flex-col justify-between">
+    <div>
+        <div class="p-4 text-xl font-bold border-b border-gray-700">
+            Admin Panel
+        </div>
+        <ul class="mt-4 space-y-2">
+            <li>
+                <a href="{{ route('dashboard') }}"
+                   class="block px-4 py-2 hover:bg-gray-700">
+                    Dashboard
+                </a>
+            </li>
+            <li>
+                <a href="#"
+                   class="block px-4 py-2 hover:bg-gray-700">
+                    Users
+                </a>
+            </li>
+            <li>
+                <a href="#"
+                   class="block px-4 py-2 hover:bg-gray-700">
+                    Settings
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="p-4">
+        @if (Auth::check())
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button type="submit"
+                class="w-full text-left px-4 py-2 bg-red-700 hover:bg-red-600 rounded">
+                Logout
+            </button>
+        </form>
+        @endif
+    </div>
+</div>
+
+
