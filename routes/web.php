527472638159464051b6b5ac06f21aa2f315ae25
anunirmani <anuttaranirmani@gmail.com>
2026-01-05 14:44:04 +0530

    Remove E mail Verification

--- routes/web.php
+++ routes/web.php
@@ -24,41 +24,37 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::middleware('can:manage-users')->group(function () {
-        Route::resource('users', UserController::class);
-    });
+    // Users
+    Route::resource('users', UserController::class);
 
-    Route::middleware('can:manage-customers')->group(function () {
-        Route::post('/customers/activate-all', [CustomerController::class, 'activateAll'])->name('customers.activateAll');
-        Route::post('/customers/deactivate-all', [CustomerController::class, 'deactivateAll'])->name('customers.deactivateAll');
-        Route::resource('customers', CustomerController::class);
-    });
+    // Customers
+    Route::post('/customers/activate-all', [CustomerController::class, 'activateAll'])->name('customers.activateAll');
+    Route::post('/customers/deactivate-all', [CustomerController::class, 'deactivateAll'])->name('customers.deactivateAll');
+    Route::resource('customers', CustomerController::class);
 
-    Route::middleware('can:manage-publications')->group(function () {
-        Route::get('/publications', [PublicationController::class, 'index'])->name('publications.index');
-        Route::get('/publications/create', [PublicationController::class, 'create'])->name('publications.create');
-        Route::post('/publications', [PublicationController::class, 'store'])->name('publications.store');
-        Route::get('/publications/{id}', [PublicationController::class, 'show'])->name('publications.show');
-        Route::get('/publications/{id}/edit', [PublicationController::class, 'edit'])->name('publications.edit');
-        Route::match(['put', 'patch'], '/publications/{id}', [PublicationController::class, 'update'])->name('publications.update');
-        Route::delete('/publications/{id}', [PublicationController::class, 'destroy'])->name('publications.destroy');
-    });
+    // Publications
+    Route::get('/publications', [PublicationController::class, 'index'])->name('publications.index');
+    Route::get('/publications/create', [PublicationController::class, 'create'])->name('publications.create');
+    Route::post('/publications', [PublicationController::class, 'store'])->name('publications.store');
+    Route::get('/publications/{id}', [PublicationController::class, 'show'])->name('publications.show');
+    Route::get('/publications/{id}/edit', [PublicationController::class, 'edit'])->name('publications.edit');
+    Route::match(['put', 'patch'], '/publications/{id}', [PublicationController::class, 'update'])->name('publications.update');
+    Route::delete('/publications/{id}', [PublicationController::class, 'destroy'])->name('publications.destroy');
 
-    Route::middleware('can:manage-copies')->group(function () {
-        Route::get('/copies', [CopyController::class, 'index'])->name('copies.index');
-        Route::get('/copies/create', [CopyController::class, 'create'])->name('copies.create');
-        Route::post('/copies', [CopyController::class, 'store'])->name('copies.store');
-        Route::get('/copies/upload', [CopyController::class, 'upload'])->name('copies.upload');
-        Route::post('/copies/upload', [CopyController::class, 'uploadStore'])->name('copies.upload.store');
-        Route::delete('/copies/{id}', [CopyController::class, 'destroy'])->name('copies.destroy');
-    });
+    // Copies
+    Route::get('/copies', [CopyController::class, 'index'])->name('copies.index');
+    Route::get('/copies/create', [CopyController::class, 'create'])->name('copies.create');
+    Route::post('/copies', [CopyController::class, 'store'])->name('copies.store');
+    Route::get('/copies/upload', [CopyController::class, 'upload'])->name('copies.upload');
+    Route::post('/copies/upload', [CopyController::class, 'uploadStore'])->name('copies.upload.store');
+    Route::delete('/copies/{id}', [CopyController::class, 'destroy'])->name('copies.destroy');
 });
 
 
